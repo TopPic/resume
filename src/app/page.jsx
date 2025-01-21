877d3cab9e09db66ed7d57dbee13ee5ed8d66c93
t.wanitcharoennan@gmail.com <t.wanitcharoennan@gmail.com>
2025-01-21 12:00:45 +0700

    add section about

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,76 +1,151 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="bg-main" >
+      <div className="bg-main h-screen flex justify-center items-center">
         <div className="container mx-auto">
-
           <div className="grid grid-cols-1 sm:grid-cols-2">
-
-            <div className="grid grid-cols-1 gap-4 mt-20 lg:mt-96 px-5">
-              <div className="mt-5 tracking-wider">
-                <h1 className="uppercase text-3xl sm:text-4xl lg:text-5xl">Thanyaporn</h1>
-                <h2 className="font-extrabold uppercase text-xl sm:text-2xl lg:text-5xl">Wanitcharoennan</h2>
-                <div className="flex space-x-5 items-center mt-5">
-                  <a href="#">
-                    <Image
-                      aria-hidden
-                      src="/images/facebook.png"
-                      alt="facebook icon"
-                      width={32}
-                      height={32}
-                    />
-                  </a>
-                  <a href="#">
-                    <Image
-                      aria-hidden
-                      src="/images/line.png"
-                      alt="line icon"
-                      width={32}
-                      height={32}
-                    />
-                  </a>
-                  <a href="#">
-                    <Image
-                      aria-hidden
-                      src="/images/instagram.png"
-                      alt="instagram icon"
-                      width={32}
-                      height={32}
-                    />
-                  </a>
-                  <a href="#">
-                    <Image
-                      aria-hidden
-                      src="/images/linkedin.png"
-                      alt="linkedin icon"
-                      width={32}
-                      height={32}
-                    />
-                  </a>
-                  <a href="#">
-                    <Image
-                      aria-hidden
-                      src="/images/github-sign.png"
-                      alt="github icon"
-                      width={32}
-                      height={32}
-                    />
-                  </a>
-                </div>
+            <div className="tracking-wider">
+              <h1 className="uppercase text-center sm:text-left text-3xl sm:text-4xl lg:text-5xl">
+                What is Lorem Ipsum
+              </h1>
+              <h2 className="font-extrabold uppercase text-center sm:text-left text-xl sm:text-2xl lg:text-5xl">
+                Lorem Ipsum
+              </h2>
+              <div className="flex space-x-5 items-center mt-5 justify-center sm:justify-start">
+                <a href="#">
+                  <Image
+                    aria-hidden
+                    src="/images/facebook.png"
+                    alt="facebook icon"
+                    width={32}
+                    height={32}
+                  />
+                </a>
+                <a href="#">
+                  <Image
+                    aria-hidden
+                    src="/images/line.png"
+                    alt="line icon"
+                    width={32}
+                    height={32}
+                  />
+                </a>
+                <a href="#">
+                  <Image
+                    aria-hidden
+                    src="/images/instagram.png"
+                    alt="instagram icon"
+                    width={32}
+                    height={32}
+                  />
+                </a>
+                <a href="#">
+                  <Image
+                    aria-hidden
+                    src="/images/linkedin.png"
+                    alt="linkedin icon"
+                    width={32}
+                    height={32}
+                  />
+                </a>
+                <a href="#">
+                  <Image
+                    aria-hidden
+                    src="/images/github-sign.png"
+                    alt="github icon"
+                    width={32}
+                    height={32}
+                  />
+                </a>
               </div>
             </div>
 
-            <div className="flex">
-              <img src="images/profile.png" alt="" />
+            <div className="bg-red-400">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor
+                voluptatibus nesciunt esse excepturi alias cumque labore debitis
+                adipisci? Minima quis voluptatum at dignissimos ducimus minus
+                quasi modi dolor quos consequatur?
+              </p>
+              {/* <img src="images/profile.png" alt="" /> */}
             </div>
-
-
           </div>
         </div>
-        {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      </div>
+      {/* About */}
+      <div className="container mx-auto">
+        <div className="grid gird-cols-1 sm:grid-cols-2 mt-10 px-5 sm:px-0 gap-4">
+          <div className="flex flex-col">
+            <p className="font-semibold text-3xl flex justify-center items-center uppercase">
+              <span className=" border-b-4 border-b-blue-500">About </span>
+            </p>
+            <p className="text-center text-gray-400 font-light">
+              t.wanitcharoennan@gamil.com
+            </p>
+            <p className="flex flex-col pt-20">
+              I am passionate about website development and have over 7 years of
+              experience in this field. Currently, I work as a Senior Web
+              Master, where I am responsible for overseeing and managing
+              websites to ensure their efficiency. This includes aspects of
+              design, development, and maintenance. In addition to website
+              development, I have a strong interest in Software Testing, which
+              plays a crucial role in enhancing the quality of systems and
+              applications. With my experience and determination to continuously
+              improve, I am committed to delivering high-quality work that adds
+              value to organizations. I am also eager to exchange ideas and
+              embrace new learning opportunities.
+            </p>
+          </div>
+          <div className="bg-red-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            sint ea quo illum maiores quos dolores, culpa officiis quae quasi
+            porro, dicta odit provident repellat, iste ipsum cum amet adipisci.
+          </div>
+        </div>
+        {/* Skill */}
+        <div className="grid grid-cols-1 sm:grid-cols-3 mt-10 px-5 sm:px-0">
+          <div className="flex flex-col">
+            <p className="font-semibold text-3xl flex justify-center items-center uppercase">
+              <span className=" border-b-4 border-b-blue-500">Title</span>
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+              eveniet vel quidem expedita maiores maxime consequuntur facilis
+              animi consectetur eaque in harum officia blanditiis tempore unde,
+              sed voluptatum possimus dolorum?
+            </p>
+          </div>
+
+          <div className="flex flex-col">
+            <p className="font-semibold text-3xl flex justify-center items-center uppercase">
+              <span className=" border-b-4 border-b-blue-500">Title</span>
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+              eveniet vel quidem expedita maiores maxime consequuntur facilis
+              animi consectetur eaque in harum officia blanditiis tempore unde,
+              sed voluptatum possimus dolorum?
+            </p>
+          </div>
+
+          <div className="flex flex-col">
+            <p className="font-semibold text-3xl flex justify-center items-center uppercase">
+              <span className=" border-b-4 border-b-blue-500">Title</span>
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+              eveniet vel quidem expedita maiores maxime consequuntur facilis
+              animi consectetur eaque in harum officia blanditiis tempore unde,
+              sed voluptatum possimus dolorum?
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -117,8 +192,6 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer> */}
-      </div>
-
     </>
   );
 }
