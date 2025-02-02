23e5a41575e5b90bb3f3ad99ec91f518e2ea17c0
TOPIC <31299339+TopPic@users.noreply.github.com>
2025-02-02 17:16:32 +0700

    add test style

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./component/Navbar";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 
 export const metadata = {
@@ -20,9 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <Navbar/>
         {children}
       </body>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,24 +1,92 @@
 'use client';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import {pp, job, Sailian} from "./component/sweetalert"
+import { pp, job, Sailian } from "./component/sweetalert"
+import styles from "./styles/Title.module.css"
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import SplitType from 'split-type';
+import Lenis from '@studio-freight/lenis';
+import AOS from "aos";
+import "aos/dist/aos.css"; // 
 
 
 export default function Home() {
+
+
+
+  useEffect(() => {
+
+    AOS.init({
+      // duration: 1000, // ความเร็วของอนิเมชัน (ในหน่วย ms)
+      once: false,     // ให้อนิเมชันทำงานครั้งเดียว (true) หรือทำซ้ำ (false)
+    });
+
+
+    gsap.registerPlugin(ScrollTrigger)
+
+    const splitTypes = document.querySelectorAll('.reveal-type')
+
+    splitTypes.forEach((char, i) => {
+
+      const bg = char.dataset.bgColor
+      const fg = char.dataset.fgColor
+
+      const text = new SplitType(char, { types: 'chars' })
+
+      gsap.fromTo(text.chars,
+        {
+          color: fg,  // เปลี่ยนเป็นสี foreground ก่อน
+        },
+        {
+          color: bg,  // แล้วเปลี่ยนเป็นสีพื้นหลัง
+          duration: 0.3,
+          stagger: 0.02,
+          scrollTrigger: {
+            trigger: char,
+            start: 'top 80%',
+            end: 'top 20%',
+            scrub: true,
+            markers: false,
+            toggleActions: 'play play reverse reverse'
+          }
+        })
+    })
+
+
+    const lenis = new Lenis()
+
+    lenis.on('scroll', (e) => {
+      console.log(e)
+    })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+  }, []);
 
   return (
     <>
       <div className="bg-main h-screen flex justify-center items-center">
         <div className="container mx-auto">
-          <div className="grid grid-cols-1 sm:grid-cols-2">
-            <div className="tracking-wider">
-              <h1 className="uppercase text-center sm:text-left text-3xl sm:text-4xl lg:text-5xl text-yellow-400">
-                What is Lorem Ipsum
-              </h1>
-              <h2 className="font-extrabold uppercase text-center sm:text-left text-xl sm:text-2xl lg:text-5xl">
-                Lorem Ipsum
-              </h2>
-              <div className="flex space-x-5 items-center mt-5 justify-center sm:justify-start">
+          <div className="grid grid-cols-1 ">
+            <div className="tracking-wider p-5">
+              <p className="font-normal text-xl text-center sm:text-left" data-aos="fade-up" data-aos-duration="1500">
+                Portfolio <br />
+                & Resume
+              </p>
+
+              <p className="uppercase text-white font-extralight text-[1.5rem] md:text-[3rem] lg:text-[7rem] text-center sm:text-left" data-aos="fade-up" data-aos-duration="3000">
+                Thanyaporn Wanitcharoennan
+              </p>
+              {/* <p className="uppercase text-center sm:text-left text-3xl sm:text-4xl lg:text-[10rem] text-white font-extralight mt-36">
+                <span className=""> </span>
+                </p> */}
+
+              {/* <div className="flex space-x-5 items-center mt-5 justify-center sm:justify-start">
                 <a href="#">
                   <Image
                     aria-hidden
@@ -64,33 +132,53 @@ export default function Home() {
                     height={32}
                   />
                 </a>
-              </div>
+              </div> */}
             </div>
 
-            <div className="bg-red-400">
+            {/* <div className="bg-red-400">
               <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor
                 voluptatibus nesciunt esse excepturi alias cumque labore debitis
                 adipisci? Minima quis voluptatum at dignissimos ducimus minus
                 quasi modi dolor quos consequatur?
               </p>
-              {/* <img src="images/profile.png" alt="" /> */}
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
       {/* About */}
+      <div className="">
+        <p className="px-20 pt-10 text-2xl flex items-center justify-center sm:justify-start" data-aos="fade-up" data-aos-duration="1500">
+          About
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256"><path d="M200,88V192a8,8,0,0,1-8,8H88a8,8,0,0,1,0-16h84.69L58.34,69.66A8,8,0,0,1,69.66,58.34L184,172.69V88a8,8,0,0,1,16,0Z"></path></svg>
+          {/* <FontAwesomeIcon icon={faArrowRight} className="ml-2 text-blue-500" /> */}
+        </p>
+      </div>
+
       <div className="container mx-auto">
-        <div className="grid gird-cols-1 sm:grid-cols-2 mt-10 px-5 sm:px-0 gap-4">
-          <div className="flex flex-col">
-            <p className="font-semibold text-3xl flex justify-center items-center uppercase">
-              <span className=" border-b-4 border-b-blue-500 text-yellow-400">Aubot</span>
-            </p>
-            <p className="text-center text-gray-400 font-light">
-              t.wanitcharoennan@gamil.com
-            </p>
-            <p className="flex flex-col pt-20">
-              I am passionate about website development and have over 7 years of
+
+        <div className={`${styles.section} section  text-[1.5rem] md:text-[3rem] lg:text-[7rem] p-5 py-10 `}>
+          <p className="reveal-type text-[1.5rem] md:text-[3rem] lg:text-[7rem] font-extralight" data-bg-color="#fff" data-fg-color="#404142">Passionate about web development with 7+ years of experience.</p>
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 px-5 sm:px-10 lg:px-20 mt-10 relative">
+          {/* Section Image */}
+          <div className="flex justify-center sm:justify-start" data-aos="fade-up" data-aos-duration="1500">
+            <Image
+              aria-hidden
+              src="/images/main_photo.jpg"
+              alt="main_photo"
+              width={800}
+              height={800}
+              className="w-full h-auto max-w-[400px] sm:max-w-[500px] md:max-w-[600px] lg:max-w-[800px]"
+            />
+          </div>
+
+          {/* Section Content */}
+          <div className="bg-neutral-600 text-white p-6 sm:p-10 shadow-lg sm:max-w-[500px] sm:absolute sm:right-[25%] sm:top-[20%] lg:top-[30%]" data-aos="fade-up" data-aos-duration="2000">
+            <h3 className="text-3xl sm:text-4xl font-bold mb-4">Thanyapoen</h3>
+            <p className="text-neutral-300 text-sm sm:text-md leading-relaxed">
+            I am passionate about website development and have over 7 years of
               experience in this field. Currently, I work as a Senior Web
               Master, where I am responsible for overseeing and managing
               websites to ensure their efficiency. This includes aspects of
@@ -103,14 +191,9 @@ export default function Home() {
               embrace new learning opportunities.
             </p>
           </div>
-          <div className="bg-red-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-            sint ea quo illum maiores quos dolores, culpa officiis quae quasi
-            porro, dicta odit provident repellat, iste ipsum cum amet adipisci.
-          </div>
         </div>
         {/* Skill */}
-        <div className="grid grid-cols-1 sm:grid-cols-3 mt-10 px-5 sm:px-0 gap-4">
+        {/* <div className="grid grid-cols-1 sm:grid-cols-3 mt-10 px-5 sm:px-0 gap-4">
           <div className="flex flex-col">
             <p className="font-semibold text-3xl flex justify-center items-center uppercase">
               <span className=" border-b-4 border-b-blue-500 text-yellow-400">Skill</span>
@@ -239,7 +322,7 @@ export default function Home() {
               sed voluptatum possimus dolorum?
             </p>
           </div>
-        </div>
+        </div> */}
       </div>
       <footer className="bg-neutral-900 w-full">
         <div className="grid grid-cols-1 sm:grid-cols-1 mt-5 p-5">
